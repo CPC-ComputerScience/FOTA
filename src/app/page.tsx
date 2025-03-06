cdbc438cd330b8cfe95981af090ea4301876bbbf
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-03-06 11:56:11 -0500

    uploaded fonts and started work on sidenav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
     <div>
+      <Sidebar />
       <h1>Fota Home</h1>
       <Link href="/test">
         <button>Test page</button>
