a4c795a8ef3e4fa9873dd1f90d3b38bc6f83a2f7
Kyle Andersen <kyleandersen279@gmail.com>
2025-03-26 11:29:16 -0400

    Updated File Structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
-      <h1>Fota Home</h1>
-      <Link href="/test">
-        <button>Test page</button>
-      </Link>
+      <h1>Home Page</h1>
     </div>
   );
 }
