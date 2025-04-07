344d451f377e3757b93a7f132632f432701b1227
Kyle Andersen <kyleandersen279@gmail.com>
2025-04-07 12:36:56 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <h1>Fota Home</h1>
       <Link href="/test">
         <button>Test page </button>
       </Link>
