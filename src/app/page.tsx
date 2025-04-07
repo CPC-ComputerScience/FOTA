eb64040b1f8f88e6e64dee239f7d95e80763ea71
nico <nico@nicos-MacBook-Pro.local>
2025-04-07 14:03:09 -0400

    fixed home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,17 @@
 import Link from "next/link";
 
-
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
 
       <Link href="/test/example">
-        <button>Test page </button>
-
-      <Link href="/test">
-        <button>Test page</button>
-
+        <button>Test Page Example</button>
       </Link>
+
       <Link href="/onboarding">
         <button>Onboarding Page</button>
       </Link>
-
     </div>
   );
 }
