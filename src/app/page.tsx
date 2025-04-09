f06c7b828b464fa8417869465d3511b4835e442f
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-04-09 10:46:13 -0400

    added video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
+"use client";
+
 import Link from "next/link";
 import Sidebar from "../components/Sidebar";
+import "./home.scss";
+import { useState, useRef } from "react";
 
 export default function Home() {
+  const [videoSource, setVideoSource] = useState(
+    "video/on-load.mp4"
+  );
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleVideoEnded = () => {
+    setVideoSource("video/idle.mp4");
+    if (videoRef.current) {
+      videoRef.current.load();
+      videoRef.current.play();
+    }
+  };
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="home-page">
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        onEnded={handleVideoEnded} 
+        className="background-video"
+      >
+        <source src={videoSource} type="video/mp4" />
+      </video>
+      {/* <h1>Home Page</h1>
 
       <Link href="/test/example">
         <button>Test Page Example</button>
@@ -12,7 +38,7 @@ export default function Home() {
 
       <Link href="/onboarding">
         <button>Onboarding Page</button>
-      </Link>
+      </Link> */}
     </div>
   );
 }
