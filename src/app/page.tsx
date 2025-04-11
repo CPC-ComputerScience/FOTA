f88311259b6e8ccd0bab46d213379e0742a1d0dd
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-04-11 11:58:36 -0400

    improved loading of video background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,44 @@
 import Link from "next/link";
 import Sidebar from "../components/Sidebar";
 import "./home.scss";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
-  const [videoSource, setVideoSource] = useState(
-    "video/on-load.mp4"
-  );
+  const [videoSource, setVideoSource] = useState("video/on-load.mp4");
   const videoRef = useRef<HTMLVideoElement>(null);
+  const idleVideoRef = useRef<HTMLVideoElement>(null);
 
   const handleVideoEnded = () => {
-    setVideoSource("video/idle.mp4");
+    if (idleVideoRef.current) {
+      idleVideoRef.current.classList.remove('hide');
+      idleVideoRef.current.classList.add('show');
+    }
     if (videoRef.current) {
-      videoRef.current.load();
-      videoRef.current.play();
+      videoRef.current.classList.remove('show');
+      videoRef.current.classList.add('hide');
     }
   };
 
   return (
     <div className="home-page">
       <video
+        ref={idleVideoRef}
+        autoPlay
+        muted
+        loop
+        className="background-video hide"
+      >
+        <source src="video/idle.mp4" type="video/mp4" />
+      </video>
+      <video
         ref={videoRef}
         autoPlay
         muted
-        onEnded={handleVideoEnded} 
-        className="background-video"
+        onEnded={handleVideoEnded}
+        loop={videoSource === "video/idle.mp4"}
+        className="background-video show"
       >
-        <source src={videoSource} type="video/mp4" />
+        <source src="video/on-load.mp4" type="video/mp4" />
       </video>
       {/* <h1>Home Page</h1>
 
