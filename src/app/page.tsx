1c369e4d61bc042607b8123f616e64527aaa0564
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-04-17 12:18:30 -0400

    rough draft of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,51 +6,24 @@ import "./home.scss";
 import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
-  const [videoSource, setVideoSource] = useState("video/on-load.mp4");
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const idleVideoRef = useRef<HTMLVideoElement>(null);
-
-  const handleVideoEnded = () => {
-    if (idleVideoRef.current) {
-      idleVideoRef.current.classList.remove('hide');
-      idleVideoRef.current.classList.add('show');
-    }
-    if (videoRef.current) {
-      videoRef.current.classList.remove('show');
-      videoRef.current.classList.add('hide');
-    }
-  };
-
   return (
     <div className="home-page">
-      <video
-        ref={idleVideoRef}
-        autoPlay
-        muted
-        loop
-        className="background-video hide"
-      >
-        <source src="video/idle.mp4" type="video/mp4" />
-      </video>
-      <video
-        ref={videoRef}
-        autoPlay
-        muted
-        onEnded={handleVideoEnded}
-        loop={videoSource === "video/idle.mp4"}
-        className="background-video show"
-      >
-        <source src="video/on-load.mp4" type="video/mp4" />
-      </video>
-      {/* <h1>Home Page</h1>
+      <div
+        id="tube-border"
+        className="tube-border tube-border-mobile"
+      ></div>
 
-      <Link href="/test/example">
-        <button>Test Page Example</button>
-      </Link>
-
-      <Link href="/onboarding">
-        <button>Onboarding Page</button>
-      </Link> */}
+      <div className="tube-shape">
+        <div className="content">
+          <h1>Festival Of The Arts</h1>
+          <br />
+          <Link className="link" href="/about">About Us</Link>
+          <br />
+          <Link className="link" href="/map">View Map</Link>
+          <br />
+          <Link className="link" href="/games">Play Games</Link>
+        </div>
+      </div>
     </div>
   );
 }
